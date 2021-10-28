ef21048e27d5cb562ebb7722f9bf88dc7fae321c
olivia <larsonog@hendrix.edu>
2021-10-28 17:07:44 -0500

    Before it wants me to delete my database

--- HalloweenSpookyUniversity2/Pages/Students/Index.cshtml.cs
+++ HalloweenSpookyUniversity2/Pages/Students/Index.cshtml.cs
@@ -7,35 +7,44 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using HalloweenSpookyUniversity2.Data;
 using HalloweenSpookyUniversity2.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace HalloweenSpookyUniversity2.Pages.Students
 {
     public class IndexModel : PageModel
     {
         private readonly SchoolContext _context;
-        public IndexModel(SchoolContext context)
+        private readonly IConfiguration Configuration;
+
+        public IndexModel(SchoolContext context, IConfiguration configuration)
         {
             _context = context;
+            Configuration = configuration;
         }
 
         public string NameSort { get; set; }
         public string DateSort { get; set; }
         public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
-
         public string AgeSort { get; set; }
 
-        public IList<Student> Students { get; set; }
 
-        public async Task OnGetAsync(string sortOrder, string searchString)
+        public PaginatedList<Student> Students { get; set; }
+
+        public async Task OnGetAsync(string sortOrder,
+            string currentFilter, string searchString, int? pageIndex)
         {
-            // using System;
+            CurrentSort = sortOrder;
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
-            AgeSort = sortOrder == "Age" ? "age_desc" : "Age";
-
-            CurrentFilter = searchString;
-
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
             IQueryable<Student> studentsIQ = from s in _context.Students
                                              select s;
             if (!String.IsNullOrEmpty(searchString))
@@ -68,8 +77,10 @@ namespace HalloweenSpookyUniversity2.Pages.Students
 
             }
 
-            Students = await studentsIQ.AsNoTracking().ToListAsync();
+            var pageSize = Configuration.GetValue("PageSize", 4);
+            Students = await PaginatedList<Student>.CreateAsync(
+                studentsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
-
+    
 }
